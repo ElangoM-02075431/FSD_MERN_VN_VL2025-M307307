5a0674ed2dc19a36602d3c76ee9ac15330a37acb
ElangoM-02075431 <elango.mofficial@gmail.com>
2025-12-24 22:13:56 +0530

    Update main.jsx

--- Project-1/frontend/src/main.jsx
+++ Project-1/frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
@@ -18,4 +18,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
+
 )
