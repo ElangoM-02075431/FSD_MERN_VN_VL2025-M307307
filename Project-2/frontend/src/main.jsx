d9d4c50daabf11158faf8b05e6385ef989044ccc
ElangoM-02075431 <elango.mofficial@gmail.com>
2026-01-01 19:16:57 +0530

    Update main.jsx

--- Project-2/frontend/src/main.jsx
+++ Project-2/frontend/src/main.jsx
@@ -5,15 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { CartProvider } from './CartContext.jsx'
 import { AuthProvider } from './AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import AppNavbar from './components/Navbar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <CartProvider>
+          <AppNavbar />   {/* Navbar appears on ALL pages */}
           <App />
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
