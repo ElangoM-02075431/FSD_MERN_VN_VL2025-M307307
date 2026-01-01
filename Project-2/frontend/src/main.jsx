e6e36b9f784ccd57422b9d10c737d741ffa258fe
ElangoM-02075431 <elango.mofficial@gmail.com>
2026-01-01 14:20:01 +0530

    Wrap with AuthProvider

--- Project-2/frontend/src/main.jsx
+++ Project-2/frontend/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { CartProvider } from './CartContext.jsx'
+import { AuthProvider } from './AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
