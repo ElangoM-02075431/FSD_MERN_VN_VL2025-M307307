3edd3a8de0db36bed5a3cd8f2681ebb0fe9d1780
ElangoM-02075431 <elango.mofficial@gmail.com>
2026-01-01 15:41:14 +0530

    Update vite.config.js

--- Project-2/frontend/vite.config.js
+++ Project-2/frontend/vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://backend-8gua.onrender.com/',  // ← your Render URL
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
