ea214e320e2750f6c836f9707ea6ee2286746b6c
ElangoM-02075431 <elango.mofficial@gmail.com>
2026-01-01 13:52:40 +0530

    Connect MongoDB in server.js

--- Project-2/backend/server.js
+++ Project-2/backend/server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Test route
 app.get('/', (req, res) => {
   res.send('Shopez Backend Live! 🛒');
 });
