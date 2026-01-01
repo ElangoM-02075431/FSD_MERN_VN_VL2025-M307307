62935c91743f3b13c8038e3c06a6c6cf8d42aa72
ElangoM-02075431 <elango.mofficial@gmail.com>
2026-01-01 14:13:48 +0530

    Wrap App with CartProvider and Router

--- Project-2/frontend/src/main.jsx
+++ Project-2/frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { CartProvider } from './CartContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
