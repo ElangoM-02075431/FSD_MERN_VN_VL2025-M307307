a181d9c3239744d01218e7c8bb51efa4707cd93c
ElangoM-02075431 <elango.mofficial@gmail.com>
2026-01-01 14:00:14 +0530

    Connect product routes

--- Project-2/backend/server.js
+++ Project-2/backend/server.js
@@ -7,8 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
+
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://root:root@bookstoremern-stack.1oyln4z.mongodb.net/')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
