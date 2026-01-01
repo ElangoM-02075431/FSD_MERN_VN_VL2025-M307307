5bb0e9ced837d515eebd8c2673248fa28bf7224a
ElangoM-02075431 <elango.mofficial@gmail.com>
2026-01-01 21:49:52 +0530

    Update main.jsx

--- Project-2/frontend/src/main.jsx
+++ Project-2/frontend/src/main.jsx
@@ -6,14 +6,20 @@ import { CartProvider } from './CartContext.jsx'
 import { AuthProvider } from './AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppNavbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <CartProvider>
-          <AppNavbar />   {/* Navbar appears on ALL pages */}
-          <App />
+          <AppNavbar />
+          <div className="min-vh-100 d-flex flex-column">
+            <div className="flex-grow-1">
+              <App />
+            </div>
+            <Footer />
+          </div>
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
