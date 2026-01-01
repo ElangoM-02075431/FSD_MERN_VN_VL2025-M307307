0d9fb81e3903c226a1ff7c92b8b637848b87a830
ElangoM-02075431 <elango.mofficial@gmail.com>
2026-01-01 14:13:07 +0530

    Add CartContext for global cart state

---
+++ Project-2/frontend/src/CartContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart(prevCart => {
+      const existing = prevCart.find(item => item._id === product._id);
+      if (existing) {
+        return prevCart.map(item =>
+          item._id === product._id 
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+      return [...prevCart, { ...product, quantity: 1 }];
+    });
+  };
+
+  const removeFromCart = (id) => {
+    setCart(prevCart => prevCart.filter(item => item._id !== id));
+  };
+
+  const cartCount = cart.reduce((sum, item) => sum + item.quantity, 0);
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, cartCount }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
